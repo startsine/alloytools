98933de8456b88462a77f8af97af410abaeca246
gradliang <gradliang@live.com>
2021-06-12 21:30:48 +0800

    update code.

---
+++ src/adump/adump.cpp
@@ -0,0 +1,49 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#ifdef _WIN32
+extern "C" __declspec(dllimport) int dumper_v1(int argc, char** argv);
+
+int wmain(int argc, WCHAR ** argv)
+{
+	int i, ret;
+	char** byteArgv;
+
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+	
+	byteArgv = (char**) malloc(sizeof(char*) * argc);
+
+	for (i = 0; i < argc; i++) 
+	{
+		size_t wlen = wcslen(argv[i]);
+		size_t blen = wlen * 4;
+		char* buffer = (char*) malloc(blen);
+		WideCharToMultiByte(CP_UTF8, 0, argv[i], -1, buffer, (int)blen, NULL, NULL);
+		byteArgv[i] = buffer;
+	}
+
+	ret = dumper_v1(argc, byteArgv);
+
+	for (i = 0; i < argc; i++)
+		free(byteArgv[i]);
+	free(byteArgv);
+	return ret;
+}
+
+#else
+
+extern "C" int dumper_v1(int argc, char** argv);
+
+int main(int argc, char** argv)
+{
+	return dumper_v1(argc, argv);
+}
+
+#endif
+
---
+++ src/alloytools1/dumper_main.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+extern "C" 
+#ifdef _WIN32
+__declspec(dllexport) 
+#endif
+int dumper_v1(int argc, char** argv)
+{
+    printf("我测试看呀");
+    return 0;
+}
+
+
+
