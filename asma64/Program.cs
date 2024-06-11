6d64794c3b9de51857bc835401955af76bfa50ac
liangquanqing <liangquanqing@coocaa.com>
2024-06-11 15:39:03 +0800

    add project

---
+++ asma64/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace asma64
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ asmla64/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace asmla64
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
