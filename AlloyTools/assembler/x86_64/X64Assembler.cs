85515e95371e31da40c1c5a2c4989589de8727bd
liangquanqing <liangquanqing@coocaa.com>
2024-07-09 18:44:57 +0800

    update  code

--- AlloyTools/assembler/x86_64/X64Assembler.cs
+++ AlloyTools/assembler/x86_64/X64Assembler.cs
@@ -3,6 +3,7 @@
 
 using AlloyTools.utils;
 using System.Collections;
+using System.Diagnostics.Metrics;
 using System.Text;
 
 namespace AlloyTools.Assembler.AMD64
@@ -26,6 +27,7 @@ namespace AlloyTools.Assembler.AMD64
             sourceLinesP = new LargeList<SourceLinePre>();
             var parser = new SourceParser(sourceLinesP, loader);
             parser.Parse();
+            //
 
 
             Hashtable ht = new Hashtable();
@@ -33,6 +35,45 @@ namespace AlloyTools.Assembler.AMD64
             //Console.WriteLine("open ok");
             //string s = Encoding.UTF8.GetString(src);
             //Console.WriteLine(s);
+        }
+
+        private void AssemblerPass1()
+        {
+            if (sourceLines == null)
+                return;
+            ulong lineCnt = 0;
+            int tokenCnt = 0;
+            int insnStartIdx = 0;                                       // 当前行中，指令的token的起始索引
+            int operandStartIdx = 0;                                    // 当前行中，操作数的token的起始索引
+            ulong lineTotal = sourceLinesP!.Count;
+            for (lineCnt = 0; lineCnt < lineTotal; lineCnt++) {
+                SourceLinePre curLine = sourceLinesP[lineCnt];
+                if (curLine.tokens == null || curLine.tokens.Count == 0)
+                    continue;
+                operandStartIdx = 0;
+                if (curLine.tokens.Count >= 2) {
+                    var token0 = curLine.tokens[0];
+                    var token1 = curLine.tokens[1];
+                    if (token1 != null) {
+                        if (token1.str == ":") {
+                            insnStartIdx = 2;
+                            operandStartIdx = 3;
+                        }
+                        else if (X64Token.isWithNamePseudoInstruction(token1.str)) {    // 是否允许name在前面的伪指令 
+                            insnStartIdx = 1;
+                            operandStartIdx = 2;
+                        }
+                    }
+                }
+
+                if (curLine.tokens != null) {
+
+                    int tokenTotal = curLine.tokens.Count;
+                    for (tokenCnt = 0; tokenCnt < tokenTotal; tokenCnt++) {
+
+                    }
+                }
+            }
         }
     }
 }
---
+++ AlloyTools/assembler/x86_64/X64Tokens.cs
@@ -0,0 +1,76 @@
+
+
+using System.Collections;
+
+namespace AlloyTools.Assembler.AMD64
+{
+    public enum X64TokenType : uint
+    {
+        Register = 0x01,                                    // 寄存器
+        Operator = 0x02,                                    // 运算符
+        Numeric = 0x04,                                     // 数字
+        Symbol = 0x08,                                      // 符号
+        String = 0x10,                                      // 字符串
+        //
+        NumericToBeUlong = 0x1000,                          // 数字已经转换为ulong
+        //
+        PseudoInstruction = 0x10000000,                     // 伪指令标志
+        DataPseudoInstruction = 0x20000000,                 // 是否产生数据的伪指令（DB,DW,DD,DQ等）
+        CpuInstruction = 0x40000000,                        // CPU真实指令标志
+        PrefixInstruction = CpuInstruction | 0x80000000,    // 前缀指令（LOCK/REPNE/REPNZ/REP/REPE/REPZ等），前缀指令同时也是CPU真实指令标志
+    }
+
+    public enum X64AsmOperator
+    {
+        None = 0,
+    }
+
+    public class X64Token
+    {
+        public X64TokenType tokenType;
+        public bool isRawData = false;                      // token的字符串是否使用byte[]原始格式
+        public string str = string.Empty;                   // token的字符串, string 类型
+        public byte[]? rawBytes = null;                     // token的字符串, byte[] 类型，token类型为String有效
+        public X64RegValue regValue;                        // 寄存器的值, tokenType 为 Register 时有效
+        public X64AsmOperator asmOperator;                  // 运算符的值, tokenType 为 Operator 时有效
+        public ulong ulongValue;                            // 数字的值, tokenType 为 Numeric 时有效
+        public ulong symbolIndex;                           // 符号在符号表中的索引, tokenType 为 Symbol 时有效
+
+        private static Hashtable htAllowNamePseudoInstruction = new Hashtable();
+
+        static X64Token()
+        {
+            initWithNamePseudoInstruction();
+        }
+
+        private static void initWithNamePseudoInstruction()
+        {
+            htAllowNamePseudoInstruction.Add("db", 1);
+            htAllowNamePseudoInstruction.Add("dw", 1);
+            htAllowNamePseudoInstruction.Add("dd", 1);
+            htAllowNamePseudoInstruction.Add("dq", 1);
+            htAllowNamePseudoInstruction.Add("proc", 1);
+            htAllowNamePseudoInstruction.Add("endp", 1);
+            htAllowNamePseudoInstruction.Add("segment_data", 1);
+            htAllowNamePseudoInstruction.Add("ends", 1);
+        }
+
+        // 判断字符串是否为数字token
+        public static bool isNumericStr(string str)
+        {
+            if (str.Length == 0)
+                return false;
+            if (str[0] >= '0' && str[0] <= '9') {
+                return true;
+            }
+            return false;
+        }
+
+        // 是否是允许带name的伪指令
+        public static bool isAllowNamePseudoInstruction(string str) {
+            return htAllowNamePseudoInstruction.ContainsKey(str);
+        }
+    }
+}
+
+
