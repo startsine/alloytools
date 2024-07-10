d63306da484c37c1ce032513f6eb99877e254185
liangquanqing <liangquanqing@coocaa.com>
2024-07-10 23:38:06 +0800

    update code.

--- AlloyTools/assembler/x86_64/X64Expression.cs
+++ AlloyTools/assembler/x86_64/X64Expression.cs
@@ -6,8 +6,24 @@ namespace AlloyTools.Assembler.AMD64
         public List<X64Token> tokens = new List<X64Token>();
 
         // 把预处理的tokens转换为表达式列表,成功返回true，失败返回false
-        public static bool ParseByPreProcessTokens(List<X64Expression> expressions, List<PreProToken> tokens, int  startIdx)
+        public static bool ParseByPreProcessTokens(List<X64Expression> expressions, List<PreProToken> tokens, int startIdx)
         {
+            if (startIdx < expressions.Count) {
+                X64Expression expr = new X64Expression();
+                for (int i = startIdx; i < tokens.Count; i++) {
+                    PreProToken token = tokens[i];
+                    if (token.str == ",") {                     // end current expression
+                        expressions.Add(expr);
+                    }
+                    else if (token.str == "\'" || token.str == "\"") {
+
+                    }
+                    else {
+                        X64Token x64Token = new X64Token();
+                        expr.tokens.Add(x64Token);
+                    }
+                }
+            }
             return true;
         }
     }
