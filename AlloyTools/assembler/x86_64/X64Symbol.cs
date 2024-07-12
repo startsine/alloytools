2e824074c5ee0f0246f2600ababc1d554b305aea
liangquanqing <liangquanqing@coocaa.com>
2024-07-12 12:01:29 +0800

    update code

--- AlloyTools/assembler/x86_64/X64Symbol.cs
+++ AlloyTools/assembler/x86_64/X64Symbol.cs
@@ -1,4 +1,6 @@
 
+using AlloyTools.utils;
+
 namespace AlloyTools.Assembler.AMD64
 {
     public enum SymboSizeType
@@ -33,8 +35,53 @@ namespace AlloyTools.Assembler.AMD64
         public SymboSizeType sizeType = SymboSizeType.None;                     // 符号的大小类型
         public SymboVisibilityType visibType = SymboVisibilityType.None;        // 可见性
         public SymbolVarType varType = SymbolVarType.None;                      // 标识是变量还是常量
-        public ulong recordIndex;                                               // 位于哪个record
+        public ulong recordIndex;                                               // 位于哪个record （section -> fragment/proc -> record）
+                                                                                // fragment/proc 是由多个连续的 record 组成
+        public ulong offsetValue;                                               // 在 record 中的偏移量 (为const时，这里存放值)
+        public string symbolName = "";                                          // 符号名
     }
 
+    public enum X64RecordType
+    {
+        None = 0,
+        Fixed,                                                                  // 固定大小记录
+        Variable                                                                // 可变大小记录
+    }
+
+    public class X64Record
+    {
+        public X64RecordType recordType;                                        // 记录类型
+        public ulong fixedSize;                                                 // 固定大小 (仅仅 recordType == Fixed 时有效)
+        public ulong fragmentIndex;                                             // 属于哪个 Fragment/Proc
+    }
+
+    public class X64Fragment
+    {
+        public string sectionName = "";                                         // 属于哪个 section，默认情况下,代码段是 ".text"，数据段是 ".data"
+
+        public X64Fragment(string secName)
+        {
+            sectionName = secName;
+        }
+    }
+
+    public class X64FragmentList
+    {
+        public long currFragmentIndex;
+        public static LargeList<X64FragmentList> fragments;
+
+        static X64FragmentList()
+        {
+            fragments = new LargeList<X64FragmentList>();
+        }
+
+        static ulong getCurrFragmentIndex()
+        {
+            if (fragments.Count == 0) {
+            }
+            return 0;
+        }
+    }
 }
+
 
