2f3978f7d27a10667658d736d368900b62d1a21d
liangquanqing <liangquanqing@coocaa.com>
2024-07-17 10:29:40 +0800

    update code.

--- AlloyTools/assembler/x86_64/X64Errors.cs
+++ AlloyTools/assembler/x86_64/X64Errors.cs
@@ -2,7 +2,11 @@
 
 namespace AlloyTools.Assembler.AMD64
 {
-
+    public class X64Errors
+    {
+        public static int errorsTotal = 0;
+        public static int warningTotal = 0;
+    }
 }
 
 
