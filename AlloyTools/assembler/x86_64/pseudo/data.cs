0db0fbdc32fa1a688aa229628a2ed49ca03f82ef
liangquanqing <liangquanqing@coocaa.com>
2024-07-23 14:36:13 +0800

    add data.cs

---
+++ AlloyTools/assembler/x86_64/pseudo/data.cs
@@ -0,0 +1,12 @@
+
+namespace AlloyTools.Assembler.AMD64.Instruction
+{
+    public class DATA : IInsnProcessor
+    {
+        public override int process(string insnStr, SourceLine sourceLine, int pass)
+        {
+            return 0;
+
+        }
+    }
+}
