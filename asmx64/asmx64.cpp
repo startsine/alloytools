be468cccfe698defec46d97039bcd3e33e83c179
liangquanqing <liangquanqing@coocaa.com>
2025-03-30 17:12:58 +0800

    改为C++

---
+++ asmx64/asmx64.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <stdio.h>
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+int main()
+{
+#ifdef _WIN32
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+    printf("测试输出中文!\n");
+}
+
