83e64079c0cb62953ce8694d36ae7667ebd84421
gradliang <gradliang@live.com>
2025-05-18 10:50:18 +0800

    修改：让命令行参数支持 UTF-8

--- asmx64/asmx64.cpp
+++ asmx64/asmx64.cpp
@@ -1,7 +1,45 @@
 #include <stdio.h>
+#ifdef _WIN32
+#include <Windows.h>
+#endif
 
-int main(int argc, char **argv)
+static int asm_main(int argc, char ** argv) 
 {
-	printf("hello world, 你好，中文输出测试。  \n");
-	return 0;
+    return 0;
+}
+
+int main(int argc, char ** argv)
+{
+    int ret = 0;
+#ifdef _WIN32
+    int i;
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+    const wchar_t * cmdline = GetCommandLineW();
+    int argsSize;
+    wchar_t ** wargList = CommandLineToArgvW(cmdline, &argsSize);
+    char ** byteArgList = new char *[argsSize + 1];
+    for (i = 0; i < argsSize; i++) {
+        size_t wlen = wcslen(wargList[i]);
+        size_t maxByteLen = (wlen + 1) * 4;             // in UTF-8, max size of one character is 4
+        char * byteArg = new char[maxByteLen];
+        BOOL usedDefaultChar;
+        WideCharToMultiByte(CP_UTF8, 0, wargList[i], wlen + 1, byteArg, maxByteLen, "_", &usedDefaultChar);
+        byteArgList[i] = byteArg;
+    }
+    byteArgList[i] = nullptr;
+    LocalFree(wargList);
+    //
+    ret = asm_main(argsSize, byteArgList);
+    
+    for (i = 0; i < argsSize; i++) {
+        delete[] byteArgList[i];
+    }
+    delete[] byteArgList;
+    
+#else
+    ret = asm_main(argsSize, byteArgList);
+#endif
+
+    return ret;
 }
