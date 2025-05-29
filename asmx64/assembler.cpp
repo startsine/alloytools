5dbc48370039e59eb859f253dc2dfd8d2019b7fa
gradliang <gradliang@live.com>
2025-05-30 00:20:39 +0800

    update code.

--- asmx64/assembler.cpp
+++ asmx64/assembler.cpp
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include "asmx64.h"
 
+using namespace std;
 
 X64Assembler::X64Assembler()
 {
@@ -33,33 +34,27 @@ void X64Assembler::assemble(const std::string & filepath)
 
 void X64Assembler::assemblePass1()
 {
+    size_t lineCnt = 0;
+    int insnStartIdx = 0;                                       // 当前行中，指令的token的起始索引
+    int operandStartIdx = 0;                                    // 当前行中，操作数的token的起始索引
 
-/*
-    if (sourceLines == null)
-    return;
-IInsnProcessor? insnProcessor = null;
-ulong lineCnt = 0;
-int insnStartIdx = 0;                                       // 当前行中，指令的token的起始索引
-int operandStartIdx = 0;                                    // 当前行中，操作数的token的起始索引
-ulong lineTotal = sourceLinesP!.Count;
-for (lineCnt = 0; lineCnt < lineTotal; lineCnt++) {
-    SourceLinePre curLine = sourceLinesP[lineCnt];
-    SourceLine parsedLine = new SourceLine();
-    parsedLine.rawContent = curLine.rawLine;
-    sourceLines.Add(parsedLine);
-    //
-    if (curLine.tokens == null || curLine.tokens.Count == 0)
-        continue;
-
-    try {
-        if (curLine.tokens.Count >= 2) {
-            var token0 = curLine.tokens[0];
-            var token1 = curLine.tokens[1];
-            if (token1 != null) {
-                if (token1.str == ":") {
+    for (auto it = sourceLinesP.begin(); it != sourceLinesP.end(); it++) {
+        lineCnt++;
+        SourceLinePrePro & curLine = *it;
+        SourceLine parsedLine;
+        //
+        parsedLine.rawContent = curLine.rawLine;
+        if (curLine.tokens.size() == 0) {
+            continue;
+        }
+        try {
+            if (curLine.tokens.size() >= 2) {
+                string & token0 = curLine.tokens[0];
+                string & token1 = curLine.tokens[1];
+                if (token1 == ":") {
                     insnStartIdx = 2;
                     parsedLine.hasLabel = true;
-                    parsedLine.labelStr = token0.str;
+                    parsedLine.labelStr = token0;
                 }
                 else if (X64Token.isAllowNamePseudoInstruction(token1.str)) {    // 是否为 允许name在前面的伪指令 
                     insnStartIdx = 1;
@@ -72,7 +67,25 @@ for (lineCnt = 0; lineCnt < lineTotal; lineCnt++) {
                     parsedLine.labelStr = "";
                 }
             }
+        } catch (...) {
         }
+
+    }
+/*
+    if (sourceLines == null)
+    return;
+
+IInsnProcessor? insnProcessor = null;
+
+
+
+ulong lineTotal = sourceLinesP!.Count;
+for (lineCnt = 0; lineCnt < lineTotal; lineCnt++) {
+
+    sourceLines.Add(parsedLine);
+
+    try {
+        
         // 找出所有前缀
         int j = insnStartIdx;
         while (j < curLine.tokens.Count) {
---
+++ asmx64/tokens.h
@@ -0,0 +1,14 @@
+
+#ifndef ASMX64_TOKENS_H
+#define ASMX64_TOKENS_H 1
+
+#include <string>
+
+class X64Token
+{
+
+};
+
+#endif // ASMX64_TOKENS_H
+
+
