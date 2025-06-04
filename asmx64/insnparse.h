1bd69970fb52580ee050266cd027d0e1e5f5bc5b
liangquanqing <liangquanqing@coocaa.com>
2025-06-04 19:21:47 +0800

    update code

--- asmx64/insnparse.h
+++ asmx64/insnparse.h
@@ -15,7 +15,7 @@ class IInsnProcessor
 {
 public:
     //int process(X64Assembler asm, string insnStr, SourceLine sourceLine, int pass) = 0;
-    InsnProcessFlag getInsnFlag() = 0;
+    virtual InsnProcessFlag getInsnFlag() = 0;
 };
 
 #endif // ASMX64_INSTRUCTION_PARSE_H
