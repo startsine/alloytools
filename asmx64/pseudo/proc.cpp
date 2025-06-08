8becb2b08de3df1e0d148836a65067855a1643ed
gradliang <gradliang@live.com>
2025-06-08 15:08:43 +0800

    update code.

--- asmx64/pseudo/proc.cpp
+++ asmx64/pseudo/proc.cpp
@@ -3,4 +3,10 @@
 #include <stddef.h>
 #include "asmx64.h"
 
+InsnProcessFlag Proc::getInsnFlag() {
+}
+
+int Proc::process(X64Assembler &assembler, std::string insnStr, SourceLine &sourceLine, int pass) {
+}
+
 
