7166e5210ef10635c595efba2f22bedf1d8e3793
gradliang <gradliang@live.com>
2025-06-28 22:34:32 +0800

    update code.

--- asmx64/common/ins_mov.cpp
+++ asmx64/common/ins_mov.cpp
@@ -54,83 +54,88 @@ InsnMov::InsnMov()
     info.op1 = MatchType::rm;
     info.opcodeFlag = OpcodeFlag_ModRM_R | OpcodeFlag_bit0Size | OpcodeFlag_RMInRight;
     opcodeInfos.push_back(info);
-
-    /*
-
-
-
-
-// MOV reg, imm == B0 +r i /s3
-info = new OpcodeInfos();
-info.opcodes = new byte[1] { 0xB0 };
-info.numberOfOperand = 2;
-info.op0 = MatchType.reg;
-info.op1 = MatchType.imm;
-info.opcodeFlag = OpcodeFlag.bit3Size | OpcodeFlag.withImm | OpcodeFlag.opcodeWithReg;
-opcodeInfos.AddLast(info);
-// MOV reg/mem, imm  == C6 /0 i /s0
-info = new OpcodeInfos();
-info.opcodes = new byte[1] { 0xC6 };
-info.numberOfOperand = 2;
-info.op0 = MatchType.rm;
-info.op1 = MatchType.imm;
-info.opcodeFlag = OpcodeFlag.bit0Size | OpcodeFlag.withImm | OpcodeFlag.ModRM_Digit;
-info.digit = 0;
-opcodeInfos.AddLast(info);
-// MOV reg/mem, segReg == 8C /r
-info = new OpcodeInfos();
-info.opcodes = new byte[1] { 0x8C };
-info.numberOfOperand = 2;
-info.op0 = MatchType.rm;
-info.op1 = MatchType.segReg;
-info.opcodeFlag = OpcodeFlag.ModRM_R;
-info.forbidInfo = MatchForbid.Forbid_8bit;              // ½ûÖ¹8bit²Ù×÷
-opcodeInfos.AddLast(info);
-// MOV segReg, reg/mem16 == 8E /r
-info = new OpcodeInfos();
-info.opcodes = new byte[1] { 0x8E };
-info.numberOfOperand = 2;
-info.op0 = MatchType.segReg;
-info.op1 = MatchType.rm;
-info.opcodeFlag = OpcodeFlag.ModRM_R | OpcodeFlag.RMInRight;
-info.forbidInfo = MatchForbid.Forbid_8bit | MatchForbid.Forbid_32bit | MatchForbid.Forbid_64bit;    // ½ûÖ¹8/32/64bit²Ù×÷
-opcodeInfos.AddLast(info);
-// MOV reg64, DRn == 0F 21 /r
-info = new OpcodeInfos();
-info.opcodes = new byte[2] { 0x0F, 0x21 };
-info.numberOfOperand = 2;
-info.op0 = MatchType.rm;
-info.op1 = MatchType.debugReg;
-info.opcodeFlag = OpcodeFlag.ModRM_R;
-info.forbidInfo = MatchForbid.Forbid_8bit | MatchForbid.Forbid_16bit | MatchForbid.Forbid_32bit | MatchForbid.Forbid_mem;   // ½ûÖ¹ÄÚ´æ²Ù×÷ºÍ8/16/32bit¼Ä´æÆ÷
-opcodeInfos.AddLast(info);
-// MOV DRn, reg64 == 0F 23 /r
-info = new OpcodeInfos();
-info.opcodes = new byte[2] { 0x0F, 0x23 };
-info.numberOfOperand = 2;
-info.op0 = MatchType.debugReg;
-info.op1 = MatchType.rm;
-info.opcodeFlag = OpcodeFlag.ModRM_R | OpcodeFlag.RMInRight;
-info.forbidInfo = MatchForbid.Forbid_8bit | MatchForbid.Forbid_16bit | MatchForbid.Forbid_32bit | MatchForbid.Forbid_mem;   // ½ûÖ¹ÄÚ´æ²Ù×÷ºÍ8/16/32bit¼Ä´æÆ÷
-opcodeInfos.AddLast(info);
-// MOV reg64, CRn == 0F 20 /r
-info = new OpcodeInfos();
-info.opcodes = new byte[2] { 0x0F, 0x20 };
-info.numberOfOperand = 2;
-info.op0 = MatchType.rm;
-info.op1 = MatchType.ctrlReg;
-info.opcodeFlag = OpcodeFlag.ModRM_R;
-info.forbidInfo = MatchForbid.Forbid_8bit | MatchForbid.Forbid_16bit | MatchForbid.Forbid_32bit | MatchForbid.Forbid_mem;   // ½ûÖ¹ÄÚ´æ²Ù×÷ºÍ8/16/32bit¼Ä´æÆ÷
-opcodeInfos.AddLast(info);
-// MOV CRn, reg64 == 0F 22 /r
-info = new OpcodeInfos();
-info.opcodes = new byte[2] { 0x0F, 0x22 };
-info.numberOfOperand = 2;
-info.op0 = MatchType.ctrlReg;
-info.op1 = MatchType.rm;
-info.opcodeFlag = OpcodeFlag.ModRM_R | OpcodeFlag.RMInRight;
-info.forbidInfo = MatchForbid.Forbid_8bit | MatchForbid.Forbid_16bit | MatchForbid.Forbid_32bit | MatchForbid.Forbid_mem;   // ½ûÖ¹ÄÚ´æ²Ù×÷ºÍ8/16/32bit¼Ä´æÆ÷
-opcodeInfos.AddLast(info);
-    */
+    // MOV reg, imm == B0 +r i /s3
+    info.reset();
+    info.opcodesSize = 1;
+    info.opcodes[0] = 0xB0;
+    info.numberOfOperand = 2;
+    info.op0 = MatchType::reg;
+    info.op1 = MatchType::imm;
+    info.opcodeFlag = OpcodeFlag_bit3Size | OpcodeFlag_withImm | OpcodeFlag_opcodeWithReg;
+    opcodeInfos.push_back(info);
+    // MOV reg/mem, imm  == C6 /0 i /s0
+    info.reset();
+    info.opcodesSize = 1;
+    info.opcodes[0] = 0xC6;
+    info.numberOfOperand = 2;
+    info.op0 = MatchType::rm;
+    info.op1 = MatchType::imm;
+    info.opcodeFlag = OpcodeFlag_bit0Size | OpcodeFlag_withImm | OpcodeFlag_ModRM_Digit;
+    info.digit = 0;
+    opcodeInfos.push_back(info);
+    // MOV reg/mem, segReg == 8C /r
+    info.reset();
+    info.opcodesSize = 1;
+    info.opcodes[0] = 0x8C;
+    info.numberOfOperand = 2;
+    info.op0 = MatchType::rm;
+    info.op1 = MatchType::segReg;
+    info.opcodeFlag = OpcodeFlag_ModRM_R;
+    info.forbidInfo = MatchForbid_8bit;              // ½ûÖ¹8bit²Ù×÷
+    opcodeInfos.push_back(info);
+    // MOV segReg, reg/mem16 == 8E /r
+    info.reset();
+    info.opcodesSize = 1;
+    info.opcodes[0] = 0x8E;
+    info.numberOfOperand = 2;
+    info.op0 = MatchType::segReg;
+    info.op1 = MatchType::rm;
+    info.opcodeFlag = OpcodeFlag_ModRM_R | OpcodeFlag_RMInRight;
+    info.forbidInfo = MatchForbid_8bit | MatchForbid_32bit | MatchForbid_64bit;    // ½ûÖ¹8/32/64bit²Ù×÷
+    opcodeInfos.push_back(info);
+    // MOV reg64, DRn == 0F 21 /r
+    info.reset();
+    info.opcodesSize = 2;
+    info.opcodes[0] = 0x0F;
+    info.opcodes[1] = 0x21;
+    info.numberOfOperand = 2;
+    info.op0 = MatchType::rm;
+    info.op1 = MatchType::debugReg;
+    info.opcodeFlag = OpcodeFlag_ModRM_R;
+    info.forbidInfo = MatchForbid_8bit | MatchForbid_16bit | MatchForbid_32bit | MatchForbid_mem;   // ½ûÖ¹ÄÚ´æ²Ù×÷ºÍ8/16/32bit¼Ä´æÆ÷
+    opcodeInfos.push_back(info);
+    // MOV DRn, reg64 == 0F 23 /r
+    info.reset();
+    info.opcodesSize = 2;
+    info.opcodes[0] = 0x0F;
+    info.opcodes[1] = 0x23;
+    info.numberOfOperand = 2;
+    info.op0 = MatchType::debugReg;
+    info.op1 = MatchType::rm;
+    info.opcodeFlag = OpcodeFlag_ModRM_R | OpcodeFlag_RMInRight;
+    info.forbidInfo = MatchForbid_8bit | MatchForbid_16bit | MatchForbid_32bit | MatchForbid_mem;   // ½ûÖ¹ÄÚ´æ²Ù×÷ºÍ8/16/32bit¼Ä´æÆ÷
+    opcodeInfos.push_back(info);
+    // MOV reg64, CRn == 0F 20 /r
+    info.reset();
+    info.opcodesSize = 2;
+    info.opcodes[0] = 0x0F;
+    info.opcodes[1] = 0x20;
+    info.numberOfOperand = 2;
+    info.op0 = MatchType::rm;
+    info.op1 = MatchType::ctrlReg;
+    info.opcodeFlag = OpcodeFlag_ModRM_R;
+    info.forbidInfo = MatchForbid_8bit | MatchForbid_16bit | MatchForbid_32bit | MatchForbid_mem;   // ½ûÖ¹ÄÚ´æ²Ù×÷ºÍ8/16/32bit¼Ä´æÆ÷
+    opcodeInfos.push_back(info);
+    // MOV CRn, reg64 == 0F 22 /r
+    info.reset();
+    info.opcodesSize = 2;
+    info.opcodes[0] = 0x0F;
+    info.opcodes[1] = 0x22;
+    info.numberOfOperand = 2;
+    info.op0 = MatchType::ctrlReg;
+    info.op1 = MatchType::rm;
+    info.opcodeFlag = OpcodeFlag_ModRM_R | OpcodeFlag_RMInRight;
+    info.forbidInfo = MatchForbid_8bit | MatchForbid_16bit | MatchForbid_32bit | MatchForbid_mem;   // ½ûÖ¹ÄÚ´æ²Ù×÷ºÍ8/16/32bit¼Ä´æÆ÷
+    opcodeInfos.push_back(info);
 }
 
