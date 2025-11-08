360fe67dd4aba057e96fcd24295e7d4c2803eeca
liangquanqing <liangquanqing@coocaa.com>
2025-11-08 14:10:52 +0800

    update code.

--- atlink/Symbol.h
+++ atlink/Symbol.h
@@ -20,7 +20,7 @@ class ObjectSymbolList
 {
 public:
     std::vector<Elf64ObjectSymbol>  symbols;
-    std::unordered_map<std::string, std::vector<size_t> > finder;         // 符号查找hash-table
+    //std::unordered_map<std::string, std::vector<size_t> > finder;         // 符号查找hash-table
     //
     ObjectSymbolList();
     ~ObjectSymbolList();
